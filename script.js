e164a342d4f57e467b3c584a8ef161aad6954a3d
Adii <162455305+adi-in-4k@users.noreply.github.com>
2024-10-13 16:19:03 +0530

    changes in js

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-console.log("script.js is working properly");
+const apiKey = "cur_live_j8MNk7xUaLiHIsuB4vzMktbjifLG2Ntyfr9vIbkQ";
+const apiUrl = `https://cors-anywhere.herokuapp.com/https://v6.exchangerate-api.com/v6/${apiKey}/latest/INR`;
 
-console.log(document.querySelector("select[name = 'country']").value);
+// async function fetchData() {
+//   const response = await fetch(apiUrl);
+//   const data = await response.json();
+//   console.log(data);
+// }
 
-// console.log(document.querySelector("#input-value").value);
-const inputVal = console.log(document.querySelector("#input-value").value);
+var inputElement = document.querySelector("#input-value");
+
+// Get the initial value of the input field
+// var value = inputElement.value;
+
 document.querySelector("#submit").addEventListener("click", () => {
   console.log("button clicked");
-  console.log(inputVal);
-  //   console.log(inputAmt);
+
+  async function fetchData() {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  }
+
+  fetchData();
+
+  // value = inputElement.value;
+  var value = inputElement.value;
+
+  //   // let inputVal = console.log(document.querySelector("#input-value").value);
+  console.log(value);
+  console.log(document.querySelector("select[name = 'country']").value);
 });
